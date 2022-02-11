597492bfb172e8c4bf6b9b146e4ff0efd5e56773
Leonardo <Leonardo@DESKTOP-FS5AL6E>
2022-02-11 15:34:59 -0300

    Adicionado banco de dados local ao projeto e criado classe ApplicationContext para se comunicar com o banco de dados atraves de codigo. Criado modelos que serão utilizados no projeto.

---
+++ CasaDoCodigo/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using CasaDoCodigo.Models;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Pedido> Pedidos { get; set; }
+        public DbSet<Cadastro> Cadastros { get; set; }
+        public DbSet<ItemPedido> ItemPedido { get; set; }
+
+        public ApplicationContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Produto>().HasKey(t => t.Id);
+        }
+    }
+}
