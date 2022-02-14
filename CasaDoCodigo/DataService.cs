8dfa3b8f10d54a33b2bfdb163668ccc3c31c1325
Leonardo <Leonardo@DESKTOP-FS5AL6E>
2022-02-14 09:18:02 -0300

    Criado metodo para leitura de um json dentro de um file.txt, deserializado e armazenado em banco de dados.

---
+++ CasaDoCodigo/DataService.cs
@@ -0,0 +1,34 @@
+﻿using CasaDoCodigo.Repositories;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CasaDoCodigo
+{
+    public class DataService : IDataService
+    {
+        private readonly ApplicationContext _context;
+        private readonly IProdutoRepository _produtoRepository;
+
+        public DataService(ApplicationContext context, IProdutoRepository produtoRepository)
+        {
+            _context = context;
+            _produtoRepository = produtoRepository;
+        }
+
+        public void InicializaDB()
+        {
+            _context.Database.EnsureCreated();
+            List<Livro> livros = GetLivros();
+            _produtoRepository.SaveProdutos(livros);
+        }
+
+
+        private static List<Livro> GetLivros()
+        {
+            var json = File.ReadAllText("livros.json");
+            var livros = JsonConvert.DeserializeObject<List<Livro>>(json);
+            return livros;
+        }
+    }
+}
