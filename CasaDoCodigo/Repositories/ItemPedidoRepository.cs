efa73a973fea845cc2e481189eb541d8b1fe8ade
Leonardo <Leonardo@DESKTOP-FS5AL6E>
2022-02-15 11:04:10 -0300

    Separado o codigo do script do carrinho e criado + interações

--- CasaDoCodigo/Repositories/ItemPedidoRepository.cs
+++ CasaDoCodigo/Repositories/ItemPedidoRepository.cs
@@ -1,16 +1,28 @@
 ﻿using CasaDoCodigo.Models;
+using System.Linq;
 
 namespace CasaDoCodigo.Repositories
 {
     public interface IItemPedidoRepository
     {
-
+        void UpdateQuantidade(ItemPedido itemPedido);
     }
 
     public class ItemPedidoRepository : BaseRepository<ItemPedido>, IItemPedidoRepository
     {
         public ItemPedidoRepository(ApplicationContext context) : base(context)
         {
+        }
+
+        public void UpdateQuantidade(ItemPedido itemPedido)
+        {
+            var itemPedidoDb = dbSet.Where(i => i.Id == itemPedido.Id).SingleOrDefault();
+
+            if(itemPedidoDb != null)
+            {
+                itemPedidoDb.AtualizaQuantidade(itemPedido.Quantidade);
+                _context.SaveChanges();
+            }
         }
     }
 }
